8a23a0fbc3287a5c6bc924c024e4998cd5eed6fa
Wania-Kazmi <waniakz019@gmail.com>
2023-03-04 00:30:20 +0500

    first commit

---
+++ 11-columns/app/page.tsx
@@ -0,0 +1,94 @@
+
+export default function Home() {
+  return (
+    <div>
+      {/* <!-- Columns --> */}
+      <h2 className="font-bold m-6">Columns 3</h2>
+      <div className="columns-3 m-4">
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+      </div>
+      <h2 className="font-bold m-6">Columns xs</h2>
+      <div className="columns-xs m-4">
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+      </div>
+      <h2 className="font-bold m-6">Columns md</h2>
+      <div className="columns-md m-4">
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+      </div>
+      <h2 className="font-bold m-6">Columns lg</h2>
+      <div className="columns-lg m-4">
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum doloribus corporis pariatur nisi dolorem recusandae in optio voluptatibus unde facilis.</p>
+      </div>
+
+      {/* <!-- Aspect Ratio --> */}
+      <h2 className="font-bold m-6">Aspect Ratio</h2>
+      <div className="columns-3">
+        <p className="aspect-video"><b>Para1 </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus enim sed eius ratione quae repellat dolores, facilis quos eligendi alias libero, cupiditate accusantium dicta itaque, perspiciatis quis aliquid impedit.</p>
+        <p className="aspect-square"><b>Para 2</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus enim sed eius ratione quae repellat dolores, facilis quos eligendi alias libero, cupiditate accusantium dicta itaque, perspiciatis quis aliquid impedit.</p>
+        {/* <p className="aspect-video"><b>Para 3</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus enim sed eius ratione quae repellat dolores, facilis quos eligendi alias libero, cupiditate accusantium dicta itaque, perspiciatis quis aliquid impedit.</p> */}
+      </div>
+
+    </div>
+  )
+}
+
+
+// <!-- Column Classes
+// columns-1	      columns: 1;
+// columns-2	      columns: 2;
+// columns-3	      columns: 3;
+// columns-4	      columns: 4;
+// columns-5	      columns: 5;
+// columns-6	      columns: 6;
+// columns-7	      columns: 7;
+// columns-8	      columns: 8;
+// columns-9	      columns: 9;
+// columns-10	    columns: 10;
+// columns-11	    columns: 11;
+// columns-12	    columns: 12;
+// columns-auto	  columns: auto;
+// columns-3xs	    columns: 16rem; /* 256px */
+// columns-2xs	    columns: 18rem; /* 288px */
+// columns-xs	    columns: 20rem; /* 320px */
+// columns-sm	    columns: 24rem; /* 384px */
+// columns-md	    columns: 28rem; /* 448px */
+// columns-lg	    columns: 32rem; /* 512px */
+// columns-xl	    columns: 36rem; /* 576px */
+// columns-2xl	    columns: 42rem; /* 672px */
+// columns-3xl	    columns: 48rem; /* 768px */
+// columns-4xl	    columns: 56rem; /* 896px */
+// columns-5xl	    columns: 64rem; /* 1024px */
+// columns-6xl	    columns: 72rem; /* 1152px */
+// columns-7xl	    columns: 80rem; /* 1280px */
+// -->
+
+// <!-- Break After
+//   break-after-auto	break-after: auto;
+//   break-after-avoid	break-after: avoid;
+//   break-after-all	break-after: all;
+//   break-after-avoid-page	break-after: avoid-page;
+//   break-after-page	break-after: page;
+//   break-after-left	break-after: left;
+//   break-after-right	break-after: right;
+//   break-after-column	break-after: column;
+// -->
+
+// <!-- Break Before
+//   reak-before-auto	break-before: auto;
+//   break-before-avoid	break-before: avoid;
+//   break-before-all	break-before: all;
+//   break-before-avoid-page	break-before: avoid-page;
+//   break-before-page	break-before: page;
+//   break-before-left	break-before: left;
+//   break-before-right	break-before: right;
+//   break-before-column	break-before: column;
+// -->
+
